dd1c9f9abeb9a6dc69a6f4a2ba893ed9f0197da0
AhmadAlii112 <ahmad.ali78643@gmail.com>
2024-02-24 17:39:54 +0500

    install vue in laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import Counter from './components/Counter.vue'
+
+const app = createApp()
+
+app.component('counter', Counter)
+app.mount('#app')
